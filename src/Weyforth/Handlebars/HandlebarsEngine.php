92a2ea465d5e375b7447257aa476d5bc687d2896
Mike Farrow <contact@mikefarrow.co.uk>
2014-03-23 06:16:57 +0000

    first commit

---
+++ src/Weyforth/Handlebars/HandlebarsEngine.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Handlebars extension to the Laravel View class.
+ *
+ * Loads and renders handlebars templates, and stores
+ * the templates in memory for inclusion in source files
+ * using the JsVars class.
+ *
+ * @author    Mike Farrow <contact@mikefarrow.co.uk>
+ * @license   Proprietary/Closed Source
+ * @copyright Mike Farrow
+ */
+
+namespace Twombolr\Handlebars;
+
+use Illuminate\View\Engines\EngineInterface;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Session;
+use Handlebars\Handlebars;
+use Weyforth\JS\JsVar;
+
+class HandlebarsEngine implements EngineInterface
+{
+
+
+    /**
+     * Constructor.
+     *
+     * @param Filesystem $files Files instance.
+     *
+     * @return void
+     */
+    public function __construct(Filesystem $files)
+    {
+        $this->files = $files;
+    }
+
+
+    /**
+     * Get the nested template.
+     *
+     * Renders template using data supplied, as well as inserting commonly
+     * required data such as errors, messages and input. Also stores
+     * unrendered template in JsVar container for inclusion later on.
+     *
+     * @param string $path Path of template.
+     * @param array  $data Data to inject into template.
+     *
+     * @return string Rendered template
+     */
+    public function get($path, array $data = array())
+    {
+        $view = $this->files->get($path);
+        $app  = app();
+        $h    = new Handlebars();
+
+        $paths = Config::get('view.paths');
+
+        $data = array_map(function ($item) {
+            return (is_object($item) && method_exists($item, 'toArray')) ?
+                $item->toArray() :
+                $item;
+        }, $data);
+
+        $standardData = array(
+            'errors' => Session::has('errors') ? Session::get('errors')->all() : null,
+            'error' => Session::has('errors') ? Session::get('errors')->first() : null,
+            'message' => Session::has('message') ? Session::get('message') : null,
+            'input' => Session::has('input') ? Session::get('input') : null,
+        );
+
+        $data = array_merge($data, $standardData);
+
+        $rendered = $h->render($view, $data);
+
+        $path = realpath($path);
+
+        foreach ($paths as $viewPath) {
+            $viewPath = realpath($viewPath);
+
+            if (!Str::endsWith($viewPath, '/')) {
+                $viewPath .= '/';
+            }
+
+            if (Str::startsWith($path, $viewPath)) {
+                $path = substr($path, strlen($viewPath));
+                break;
+            }
+        }
+
+        $pathNoExt = explode('.', $path);
+        array_pop($pathNoExt);
+
+        JsVar::container('templates')->add(str_replace('/', '.', implode('.', $pathNoExt)), $view);
+
+        return $rendered;
+    }
+
+
+}
---
+++ src/Weyforth/Handlebars/HandlebarsServiceProvider.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Handlebars Service Provider.
+ *
+ * @author    Mike Farrow <contact@mikefarrow.co.uk>
+ * @license   Proprietary/Closed Source
+ * @copyright Mike Farrow
+ */
+
+namespace Twombolr\Handlebars;
+
+use Illuminate\Support\ServiceProvider;
+
+class HandlebarsServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var boolean
+     */
+    protected $defer = false;
+
+
+    /**
+     * Register the service provider.
+     *
+     * Extend view class to provide our custom loader.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $app = $this->app;
+
+        $app->extend('view.engine.resolver', function ($resolver, $app) {
+            $resolver->register('handlebars', function () use ($app) {
+                return $app->make('Twombolr\Handlebars\HandlebarsEngine');
+            });
+
+            return $resolver;
+        });
+
+        $app->extend('view', function ($env, $app) {
+            $env->addExtension('handlebars', 'handlebars');
+
+            return $env;
+        });
+    }
+
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('handlebars');
+    }
+
+
+}
